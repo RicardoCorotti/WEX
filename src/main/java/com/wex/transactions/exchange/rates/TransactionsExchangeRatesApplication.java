d02d952007c41dc5b04a8e9323d9c06376b5527c
Ricardo Corotti <rcorotti@gmail.com>
2023-10-18 18:04:47 -0300

    feat: Initial Configuration

---
+++ src/main/java/com/wex/transactions/exchange/rates/TransactionsExchangeRatesApplication.java
@@ -0,0 +1,13 @@
+package com.wex.transactions.exchange.rates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsExchangeRatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsExchangeRatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wex/transactions/exchange/rates/TransactionsExchangeRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wex.transactions.exchange.rates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsExchangeRatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
